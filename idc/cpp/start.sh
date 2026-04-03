71b72cd950772b532a14391f1e21620e04779163
alex <thealbertmak@gmail.com>
2026-04-03 12:02:59 +0800

    完成程序监控调度模块

--- idc/cpp/start.sh
+++ idc/cpp/start.sh
@@ -6,8 +6,8 @@
 # 生成气象站点观测的分钟数据，每1分钟运行一次, 由调度模块启动。
 /project/tools/bin/procctl 60 /project/idc/bin/crtsurfdata /project/idc/ini/stcode.ini /tmp/idc/surfdata /log/idc/crtsurfdata.log csv,xml,json
 
-# 清理原始的气象观测数据目录（/tmp/idc/surfdata）中的历史数据文件。
+# 定期300s调用一次deletefiles来清理0.02天前生成的观测数据（/tmp/idc/surfdata）
 /project/tools/bin/procctl 300 /project/tools/bin/deletefiles /tmp/idc/surfdata "*" 0.02
 
-# 压缩后台服务程序的备份日志。
+# 定期300s调用一次gzipfiles来压缩0.02天前生成的日志文件（/log/idc）
 /project/tools/bin/procctl 300 /project/tools/bin/gzipfiles /log/idc "*.log.20*" 0.02
--- tools/cpp/deletefiles.cpp
+++ tools/cpp/deletefiles.cpp
@@ -1,19 +1,26 @@
+/**
+ * @file deletefiles.cpp
+ * @author Alexandr Mak (alxndrmak@google.com)
+ * @brief {这是一个工具程序，用来清理n天前的历史数据文件或日志文件。}
+ * @version 0.1
+ * @date 2026-04-03
+ *
+ * @copyright Copyright (c) 2026
+ *
+ */
 #include "_public.h"
 using namespace idc;
 
-// 添加进程的心跳
+// 添加进程的心跳,删除文件时间短，不需要更新心跳
 cpactive pactive;
 
-// 程序退出，信号2，15的处理函数
-void EXIT(int sig);
+void app_exit(const int sig);
 
 int main(int argc, char *argv[]) {
-  // 0-1 程序参数检查，及使用帮助
   if (argc != 4) {
     printf("\n");
-
-    printf(
-        "Using:/project/tools/bin/deletefiles pathname matchstr timeout\n\n");
+    printf("Using: this_program_name delete_files_directory_path "
+           "file_matched_rule time_days_ago\n\n");
     printf("Example:/project/tools/bin/deletefiles /tmp/idc/surfdata "
            "\"*.xml,*.json\" 0.01\n");
     cout
@@ -24,7 +31,6 @@ int main(int argc, char *argv[]) {
     printf(
         "Example4:/project/tools/bin/procctl 300 project/tools/bin/deletefiles "
         "/tmp/idc/surfdata \"*.xml,*json\" 0.01\n\n");
-
     printf("这是一个工具程序，用来清理历史数据文件或日志文件。\n");
     printf("本程序会把pathname目录及其子目录中timeout天之前，且符合matchstr条件"
            "的文件删除掉，timeout可以是小数。\n");
@@ -33,27 +39,27 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
-  // 0-2 处理程序退出：忽略全部信号，关闭IO, 设置信号处理函数
   closeioandsignal(true); // 调试时可以注释掉这行代码
-  signal(SIGINT, EXIT);
-  signal(SIGTERM, EXIT);
+  signal(SIGINT, app_exit);
+  signal(SIGTERM, app_exit);
 
   // 将进程的信息加入到共享内存中
   pactive.addpinfo(30, "deletefiles"); // 每30秒更新一次心跳
 
-  // 1-获取被定义为历史数据文件的时间点
+  // TODO:1-获取被定义为历史数据文件的时间点(多少天前的文件被定义为历史数据文件)
+  // ltime1(fmt,offset), offset为0表当前时间，负数表过去，正数表未来
   string strtimeout =
       ltime1("yyyymmddhh24miss", 0 - (int)(atof(argv[3]) * 24 * 60 * 60));
-  // cout<<"strtimeout="<<strtimeout<<endl; //调试代码
 
-  // 2-打开目录
+  // TODO:2-打开目录
   cdir dir;
   if (dir.opendir(argv[1], argv[2], 10000, true, false) == false) {
     printf("dir.opendir(%s) failed.\n", argv[1]);
     return -1;
   }
 
-  // 3-遍历目录中的文件，删除符合条件的历史数据文件
+  // TODO:3-遍历目录中的文件，删除符合条件的历史数据文件
+  // 使用remove函数删除文件，成功返回0，失败返回-1
   while (dir.readdir() == true) {
     if (dir.m_mtime < strtimeout) {
       if (remove(dir.m_ffilename.c_str()) == 0) {
@@ -68,8 +74,7 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-void EXIT(int sig) {
+void app_exit(const int sig) {
   printf("程序退出，sig=%d\n\n", sig);
-
   exit(0);
 }
